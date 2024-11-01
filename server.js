d8933368f028e5df408691a75470a25e0fc59f02
Suraj S <surajsooraj26@gmail.com>
2024-11-01 12:20:43 +0530

    database connections

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+require("dotenv").config();
 
-app.listen(3000, () =>{
-    console.log("Server connected successfully");
-})
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.log("Error in Database Connection");
+  });
+
+app.listen(3000, () => {
+  console.log("Server connected successfully");
+});
